843b7a4176c71396b0b5ff27d645a3a07a4c0413
Jean <jeanncavalcante@gmail.com>
2022-10-04 18:59:31 -0300

    add firebase

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4ttuWz8SqHyqS8rIxjHeifDdBR35L730",
+  authDomain: "curso-2d276.firebaseapp.com",
+  projectId: "curso-2d276",
+  storageBucket: "curso-2d276.appspot.com",
+  messagingSenderId: "239155791838",
+  appId: "1:239155791838:web:1f81a3f8c95ce88dcb3f4f",
+  measurementId: "G-MJ197DDXX9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
